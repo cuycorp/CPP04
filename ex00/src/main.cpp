b472bd7a8667f05d6bc469a94eefc8aa771e42c2
cuycorp <cuycorp@gmail.com>
2025-11-04 11:43:54 +0100

    added direct instance tests, and fixed order of tests

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,27 +6,43 @@
 
 int	main(void)
 {
-	const Animal *meta = new Animal();
-	const Animal *j = new Dog();
-	const Animal *i = new Cat();
-
-
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); // will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	delete meta;
-	delete i;
-	delete j;
-	std::cout << "-----------------------------------------------" << std::endl;
-	std::cout << "Wrong classes test" << std::endl;
-	std::cout << "-----------------------------------------------" << std::endl;
-	const WrongAnimal *a = new WrongAnimal();
-	const WrongAnimal *b = new WrongCat();
-	a->makeSound(); 
-	b->makeSound();
-	delete a;
-	delete b;
+	std::cout << std::endl;
+	std::cout << "-------------------- Pointers --------------------" << std::endl;
+	{
+		const Animal *meta = new Animal();
+		const Animal *j = new Dog();
+		const Animal *i = new Cat();
+		std::cout << "-------------------- After initialization --------------------" << std::endl;
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		std::cout << "-------------------- Making sound --------------------" << std::endl;
+		j->makeSound(); // will output the cat sound!
+		i->makeSound();
+		meta->makeSound();
+		std::cout << "-------------------- Deleting objects --------------------" << std::endl;
+		delete meta;
+		delete j;
+		delete i;
+		std::cout << "-----------------------------------------------" << std::endl;
+		std::cout << "Wrong classes test" << std::endl;
+		std::cout << "-----------------------------------------------" << std::endl;
+		const WrongAnimal *a = new WrongAnimal();
+		const WrongAnimal *b = new WrongCat();
+		a->makeSound();
+		b->makeSound();
+		delete a;
+		delete b;
+	}
+	std::cout << std::endl;
+	std::cout << "-------------------- Direct instance --------------------" << std::endl;
+	{
+		Animal animalejo;
+		Cat gato;
+		Dog perro;
+		std::cout << "-------------------- Making sound --------------------" << std::endl;
+		animalejo.makeSound();
+		gato.makeSound();
+		perro.makeSound();
+	}
 	return (0);
 }
